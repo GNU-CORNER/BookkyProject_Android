e1e8601dcbf3b9561f678c9722d15fa2e1835964
Changhyung <pck4949@naver.com>
2022-03-17 19:54:35 +0900

    Create HomeFragment.kt

---
+++ app/src/main/java/com/example/bookkyandroid/ui/fragment/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.example.bookkyandroid.ui.fragment.home
+
+import android.os.Bundle
+import android.view.View
+import com.example.bookkyandroid.R
+import com.example.bookkyandroid.config.BaseFragment
+import com.example.bookkyandroid.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        }
+    }
