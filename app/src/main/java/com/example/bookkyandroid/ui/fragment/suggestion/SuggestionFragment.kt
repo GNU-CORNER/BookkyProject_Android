8e0dc5924115d8ae68ac8beb0e3058d5349e8ccf
Changhyung <pck4949@naver.com>
2022-03-17 19:54:29 +0900

    Create SuggestionFragment.kt

---
+++ app/src/main/java/com/example/bookkyandroid/ui/fragment/suggestion/SuggestionFragment.kt
@@ -0,0 +1,16 @@
+package com.example.bookkyandroid.ui.fragment.suggestion
+
+import android.os.Bundle
+import android.view.View
+import com.example.bookkyandroid.R
+import com.example.bookkyandroid.config.BaseFragment
+import com.example.bookkyandroid.databinding.FragmentSuggestionBinding
+
+
+class SuggestionFragment : BaseFragment<FragmentSuggestionBinding>(FragmentSuggestionBinding::bind, R.layout.fragment_suggestion) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
