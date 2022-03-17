3a9651ccf7744880b4cd0935e5c9b7fbf37bb7a8
Changhyung <pck4949@naver.com>
2022-03-17 19:54:31 +0900

    Create SearchFragment.kt

---
+++ app/src/main/java/com/example/bookkyandroid/ui/fragment/search/SearchFragment.kt
@@ -0,0 +1,16 @@
+package com.example.bookkyandroid.ui.fragment.search
+
+import android.os.Bundle
+import android.view.View
+import com.example.bookkyandroid.R
+import com.example.bookkyandroid.config.BaseFragment
+import com.example.bookkyandroid.databinding.FragmentSearchBinding
+
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
