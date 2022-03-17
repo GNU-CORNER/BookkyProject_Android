24a5df944a8cf9feaee60661973e736a6216027a
Changhyung <pck4949@naver.com>
2022-03-17 19:54:58 +0900

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/bookkyandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/bookkyandroid/ExampleInstrumentedTest.kt
@@ -1,11 +1,11 @@
 package com.example.bookkyandroid
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
